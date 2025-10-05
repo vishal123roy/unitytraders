ef78186d1fade3cc681f1a60da809aa245b041aa
Vishnu P <pvishnukakkodi@gmail.com>
2025-10-05 14:19:56 +0530

    Vishnu commited before host

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use("/api/customers", customerRoutes);
 
 connectDB();
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-}
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
